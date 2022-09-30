1c69b3ab51fd0bcbf0cd846da5c0062fefeedddf
Diogo Valente Martins <dvmartins@student.dei.uc.pt>
2022-09-30 17:35:40 +0100

    final

--- src/main/java/com/example/tutorial/protos/Main.java
+++ src/main/java/com/example/tutorial/protos/Main.java
@@ -1,26 +1,42 @@
 package com.example.tutorial.protos;
 
 import com.github.javafaker.Faker;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Marshaller;
+import org.example.ListProfessors;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
 
+
         //System.out.println(john);
+        long ini2 = System.currentTimeMillis();
+
         Professors prof = dataGenerate();
+
+        long fini2 = System.currentTimeMillis();
+        System.out.println("Time: "+ (fini2 - ini2));
+
+        long ini = System.currentTimeMillis();
         FileOutputStream output = new FileOutputStream("testFile");
         prof.writeTo(output);
         output.close();
+        long fini = System.currentTimeMillis();
+        System.out.println("Time: "+ (fini - ini));
 
         // Read the existing address book.
+        long ini1 = System.currentTimeMillis();
         Professors addressBook =
                 Professors.parseFrom(new FileInputStream("testFile"));
-        System.out.println(addressBook);
+        long fini1 = System.currentTimeMillis();
+        System.out.println("Time: "+ (fini1 - ini1));
     }
 
     static Professors dataGenerate() {
@@ -30,17 +46,36 @@ public class Main {
         Student.Builder student = Student.newBuilder();
         Faker faker = new Faker();
 
-        for(int i = 0; i < 2 ; i++){
+        Random rand = new Random();
+
+
+        for(int i = 0; i < 10 ; i++){
            professor.setId((faker.idNumber().valid()));
            professor.setName(faker.name().firstName() + " " + faker.name().lastName());
-           professor.setEmail(faker.internet().emailAddress()) ;
            professor.setAddress(faker.address().fullAddress());
            professor.setPhoneNumber(faker.phoneNumber().cellPhone());
-           for(int j = 0; j < 3; j++){
+           String formattedDate = String.format("%1$tb %1$te, %1$tY %1$tI:%1$tM %1$Tp", faker.date().birthday());
+           professor.setBirthDate(formattedDate);
+           formattedDate = String.format("%1$tb %1$te, %1$tY %1$tI:%1$tM %1$Tp", faker.date().birthday());
+           professor.setBirthDate(formattedDate);
+           for(int j = 0; j < 1000; j++){
                student.setId((faker.idNumber().valid()));
                student.setName(faker.name().firstName() + " " + faker.name().lastName());
                student.setAddress(faker.address().fullAddress());
+               student.setPhoneNumber(faker.phoneNumber().cellPhone());
+               formattedDate = String.format("%1$tb %1$te, %1$tY %1$tI:%1$tM %1$Tp", faker.date().birthday());
+               student.setBirthDate(formattedDate);
+               formattedDate = String.format("%1$tb %1$te, %1$tY %1$tI:%1$tM %1$Tp", faker.date().birthday());
+               student.setRegistrationDate(formattedDate);
                professor.addStudent(student);
+               int random = rand.nextInt(2);
+               if(random == 0 ){
+                   student.setGender("M");
+               }
+               else{
+                   student.setGender("F");
+               }
+
            }
 
            professors.addProfessor(professor);
--- src/main/java/org/example/ObjectToXml.java
+++ src/main/java/org/example/ObjectToXml.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.example.tutorial.protos.Professors;
+import com.github.javafaker.Faker;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
@@ -11,6 +12,7 @@ import java.io.FileOutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -18,40 +20,101 @@ import java.util.zip.GZIPOutputStream;
 public class ObjectToXml {
     public static void main(String[] args) throws Exception{
 
-        Student stud1=new Student("20182728","Alberto","918172616","M","25/10/2000","25/10/2021","Rua benfica");
-        Student stud2=new Student("20182720","Joao","918672616","M","25/10/2000","25/10/2021","Rua benfica");
-        Student stud3=new Student("20182729","Diogo","918175616","M","25/10/2000","25/10/2021","Rua benfica");
-        ArrayList <Student> aux2 = new ArrayList<>();
-        aux2.add(stud1);
-        aux2.add(stud2);
-        aux2.add(stud3);
-        Professor prof=new Professor("2018276713","Jose", "20/10/1986", "918262734","Rua Vale da Azenha",aux2);
 
 
-        ArrayList <Professor> aux = new ArrayList<>();
-        aux.add(prof);
-        ListProfessors list = new ListProfessors(aux);
+        ArrayList<Professor> listProf = new ArrayList<>();
+        String profName;
+        String profId;
+        String profAddress;
+        String profNumber;
+        String profBithDate;
 
-        Professors addressBook =
-                Professors.parseFrom(new FileInputStream("testFile"));
-        System.out.println(addressBook.getProfessorList().get(0).getStudentList());
+        String studentId;
+        String studentName;
+        String studentAddress;
+        String studentPhone;
+        String studentBirthDate;
+        String studentRegistrationDate;
+
+        Faker faker = new Faker();
+
+        Random rand = new Random();
+
+        for(int i = 0; i < 10 ; i++) {
+            ArrayList <Student> aux2 = new ArrayList<>();
+            profId = faker.idNumber().valid();
+            profName = (faker.name().firstName() + " " + faker.name().lastName());
+            profAddress = (faker.address().fullAddress());
+            profNumber = (faker.phoneNumber().cellPhone());
+            String formattedDate = String.format("%1$tb %1$te, %1$tY %1$tI:%1$tM %1$Tp", faker.date().birthday());
+            formattedDate = String.format("%1$tb %1$te, %1$tY %1$tI:%1$tM %1$Tp", faker.date().birthday());
+            profBithDate = (formattedDate);
+            Professor prof = new Professor();
+            prof.setName(profName);
+            prof.setId(profId);
+            prof.setAddress(profAddress);
+            prof.setPhoneNumber(profNumber);
+            prof.setBirthDate(profBithDate);
+            Student student = new Student();
+
+            for (int j = 0; j < 1000; j++) {
+
+                studentId = ((faker.idNumber().valid()));
+                studentName = (faker.name().firstName() + " " + faker.name().lastName());
+                studentAddress = (faker.address().fullAddress());
+                studentPhone = (faker.phoneNumber().cellPhone());
+                formattedDate = String.format("%1$tb %1$te, %1$tY %1$tI:%1$tM %1$Tp", faker.date().birthday());
+                studentBirthDate = (formattedDate);
+                formattedDate = String.format("%1$tb %1$te, %1$tY %1$tI:%1$tM %1$Tp", faker.date().birthday());
+                studentRegistrationDate = (formattedDate);
+
+                student.setId(studentId);
+                student.setName(studentName);
+                student.setAddress(studentAddress);
+                student.setphoneNumber(studentPhone);
+                student.setBirthDate(studentBirthDate);
+                student.setRegistrationDate(studentRegistrationDate);
+
+                int random = rand.nextInt(2);
+                if (random == 0) {
+                    student.setGender("M");
+                } else {
+                    student.setGender("F");
+                }
+                aux2.add(student);
+
+
+            }
+            prof.setStudentList(aux2);
+            listProf.add(prof);
+
+        }
+
+        ListProfessors list = new ListProfessors(listProf);
+
 
 
         //java object to xml
+        long ini = System.currentTimeMillis();
         JAXBContext contextObj = JAXBContext.newInstance(ListProfessors.class);
         Marshaller marshallerObj = contextObj.createMarshaller();
         marshallerObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshallerObj.marshal(list,new FileOutputStream("student.xml"));
+       long fini = System.currentTimeMillis();
+       System.out.println("Time: "+ (fini - ini));
 
         //xml to java object
+        long ini1 = System.currentTimeMillis();
         File file = new File("student.xml");
         JAXBContext jaxbContext = JAXBContext.newInstance(ListProfessors.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         ListProfessors listp= (ListProfessors) jaxbUnmarshaller.unmarshal(file);
-        //System.out.println(listp.getList().get(0).getStudentList().get(0).getName());
+        long fini1 = System.currentTimeMillis();
+        System.out.println("Time: "+ (fini1 - ini1));
 
 
         // XML compressed with gzip
+        long ini2 = System.currentTimeMillis();
         Path source = Paths.get("student.xml");
         Path target = Paths.get("student.xml.gz");
         try (GZIPOutputStream gos = new GZIPOutputStream(
@@ -66,9 +129,12 @@ public class ObjectToXml {
             }
 
         }
+        long fini2 = System.currentTimeMillis();
+        System.out.println("Time: "+ (fini - ini) + (fini2 - ini2));
 
 
         //decompressed gzip
+        long ini3 = System.currentTimeMillis();
         Path source1 = Paths.get("student.xml.gz");
         Path target1 = Paths.get("studentDecompressed.xml");
 
@@ -84,6 +150,8 @@ public class ObjectToXml {
             }
 
         }
+        long fini3 = System.currentTimeMillis();
+        System.out.println("Time: "+ (fini1 - ini1) + (fini3 - ini3));
 
     }
 }
